25568be883ff32ff2cbf30cf7a354fbce2965a29
Russ Painter <russp@geekymonkey.com>
2023-03-27 18:39:06 +0100

    More source graphics and a font

--- AutoLoad/Delay.gd
+++ AutoLoad/Delay.gd
@@ -1,6 +1,16 @@
 extends Node
 
-# Pause for some time
-func Seconds(seconds: float):
-	await get_tree().create_timer(seconds).timeout
 
+## Wait for some Seconds
+func Seconds(sec: float) -> void:
+	await get_tree().create_timer(sec).timeout
+
+
+## Wait for the next frame
+func NextFrame() -> void:
+	await get_tree().process_frame
+
+
+## Wait for the next physics frame
+func NextPhysicsFrame() -> void:
+	await get_tree().physics_frame
